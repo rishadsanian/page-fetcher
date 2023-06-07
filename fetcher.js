2637f4f5b1590189780ea6aefbd09daa506ba467
Rishad Alam <rishad.s.a@gmail.com>
2023-06-07 15:42:23 -0400

    typo fix

--- fetcher.js
+++ fetcher.js
@@ -2,19 +2,19 @@ const request = require("request");
 const fs = require("fs");
 
 const getInput = () => {
+  
   const input = process.argv.slice(2);
   const url = input[0];
   const filePath = input[1];
+
   request(url, (error, response, body) => {
-    //console.log("error:", error);
-    //console.log("statusCode:", response && response.statusCode);
-    //console.log("body:", body);
+
     fs.writeFile(filePath, body, (err) => {
       if (!error === 200) {
         console.error(err);
       }
       console.log(
-        "Downloaded and saved " + body.length + "bytes to " + filePath
+        "Downloaded and saved " + body.length + " bytes to " + filePath
       );
     });
   });
